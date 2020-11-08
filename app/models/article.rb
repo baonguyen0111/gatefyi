d544825a0cf7431fac6c69fac423f46e5b151e8a
Ubuntu <ubuntu@ip-172-31-58-164.ec2.internal>
2020-11-08 02:04:37 +0000

    Allow users to select state

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+	validates :company, :industry_type, :state, :city, :interview_exp, :word_exp, presence:true
+	validates :compensation, numericality: {only_integer: true}
 end

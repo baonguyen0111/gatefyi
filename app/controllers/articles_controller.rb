4b9c0375c985001a8a80ec17c18bf161c343587c
Ubuntu <ubuntu@ip-172-31-11-202.ec2.internal>
2020-12-08 00:58:53 +0000

    One step left

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,12 @@ class ArticlesController < ApplicationController
 		most_active_poster.each do |key, value|
 			hash[key.to_i] = (value.to_i + most_active_commenter[key.to_i].to_i)
 		end
-		most_active_id = hash.max_by{|k,v| v}[0]
-		@active_user = User.find(most_active_id)
+		if hash.empty? 
+ 			@active_user = ""
+ 		else
+ 			most_active_id = hash.max_by{|k,v| v}[0]
+ 			@active_user = User.find(most_active_id)
+ 		end
 		queries = {"company" => :company, "industy" => :industry_type, "location" => :state, "salary" => :compensation, "upvotes" => :upvotes}
 		#if a sorting param has been selected
 		if queries.key? params[:query]
@@ -22,194 +26,173 @@ class ArticlesController < ApplicationController
 	    	else
 	    		session[params[:query]] = ' ASC'
 	    	end
+	    	#preserve previous filter before sorting
+	    	
 	        #Sort and update where needed
 	        @articles = Article.getApprovedArticles.paginate(:page => params[:page], :per_page => 4).reorder(queries[params[:query]].to_s + session[params[:query]])
 	        session[:prev] = params[:query]
 	        params[:query] = nil
-			@arr = [@active_user, @articles, nil]
+			@arr = [@active_user, @articles]
 		#if filtered
 		elsif params[:filter]
-			#byebug
-			if params[:popup]
-				@select = []
-				filter = params[:filter]
-				if filter == "location"
-					@select_states = Article.state_filter
-					@select_city = Article.city_filter
-					@select = [@select_states, @select_city]
-				elsif filter == "company"
-					@select_company = Article.company_filter
-					@select = @select_company;
-					#puts @select_company
+			filter = params[:filter]
+			session[:filter_by] = filter
+			#filter type
+			@articles = Article.getApprovedArticles
+			if filter == "location"
+				#city and state
+				city = params[city]
+				state = params[state]
+				if city != "" && state != ""
+					session[:filter_value] = [city, state]
+					if session[:prev] == nil
+						@articles = @articles.where('state = ?', state).where('city = ?', city)
+						@arr = [@active_user, @articles]
+					#if a param was previous selcted, we should kepp follwing it when going to a different page
+					else
+						@articles = @articles.where('state = ?', state).where('city = ?', city).reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
+					end
+				elsif city != ""
+					session[:filter_value] = city
+					if session[:prev] == nil
+						@articles = @articles.where('city = ?', city)
+						@arr = [@active_user, @articles]
+					#if a param was previous selcted, we should kepp follwing it when going to a different page
+					else
+						@articles = @articles.where('city = ?', city).reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
+					end
+				elsif state != ""
+					session[:filter_value] = state
+					if session[:prev] == nil
+						@articles = @articles.where('state = ?', state)
+						@arr = [@active_user, @articles]
+					#if a param was previous selcted, we should kepp follwing it when going to a different page
+					else
+						@articles = @articles.where('state = ?', state).reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
+					end
 				else
-					@select_industry = Article.industry_filter
-					@select = @select_industry
-				end
-				if session[:prev] == nil
-					@articles = Article.getApprovedArticles.paginate(:page => params[:page], :per_page => 4)
-					@arr = [@active_user, @articles, @select]
-					respond_to do |format|
-    					format.html
-    					format.json {render 'popup.js.erb'} if request.xhr?
-    				end
+					session[:filter_by] = nil
+					if session[:prev] == nil
+						@articles = @articles
+						@arr = [@active_user, @articles]
 				#if a param was previous selcted, we should kepp follwing it when going to a different page
+					else
+						@articles = @articles.reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
+					end
+				end
+			end
+			if filter == "salary"
+				#low and high
+				low = params["low_salary"]
+				high = params["high_salary"]
+				if low != "" && high != ""
+					session[:filter_value] = [low, high]
+					if session[:prev] == nil
+						@articles = @articles.where('compensation >= ?', low).where('compensation <= ?', high)
+						@arr = [@active_user, @articles]
+					#if a param was previous selcted, we should kepp follwing it when going to a different page
+					else
+						@articles = @articles.where('compensation >= ?', low).where('compensation <= ?', high).reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
+					end
+				elsif low != ""
+					session[:filter_value] = low
+					if session[:prev] == nil
+						@articles = @articles.where('compensation >= ?', low)
+						@arr = [@active_user, @articles]
+					#if a param was previous selcted, we should kepp follwing it when going to a different page
+					else
+						@articles = @articles.where('compensation >= ?', low).reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
+					end
+				elsif high != ""
+					session[:filter_value] = high
+					if session[:prev] == nil
+						@articles = @articles.where('compensation >= ?', 0).where('compensation <= ?', high)
+						@arr = [@active_user, @articles]
+					#if a param was previous selcted, we should kepp follwing it when going to a different page
+					else
+						@articles = @articles.where('compensation >= ?', 0).where('compensation <= ?', high).reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
+					end
 				else
-					@articles = Article.getApprovedArticles.paginate(:page => params[:page], :per_page => 4).reorder(queries[session[:prev]].to_s + session[session[:prev]])
-					@arr = [@active_user, @articles, @select]
-					respond_to do |format|
-    					format.html
-    					format.json {render 'popup.js.erb'} if request.xhr?
-    				end
-				end
-			else
-				@articles = Article.getApprovedArticles.paginate(:page => params[:page], :per_page => 4)
-				filter = params[:filter]
-				#filter type
-				@arr = [@active_user, @articles, nil]
-				if filter == "location"
-					#city and state
-					city = params[city]
-					state = params[state]
-					if (city != "undefined") && (state != "undefined")
-						if session[:prev] == nil
-							@articles = @articles.where('state = ?', state).where('city = ?', city)
-							@arr = [@active_user, @articles, nil]
+					session[:filter_by] = nil
+					if session[:prev] == nil
+						@articles = @articles
+						@arr = [@active_user, @articles]
 					#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.where('state = ?', state).where('city = ?', city).reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
-					elsif city != "undefined"
-						if session[:prev] == nil
-							@articles = @articles.where('city = ?', city)
-							@arr = [@active_user, @articles, nil]
-					#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.where('city = ?', city).reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
-					elsif state != "undefined"
-						if session[:prev] == nil
-							@articles = @articles.where('state = ?', state)
-							@arr = [@active_user, @articles, nil]
-					#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.where('state = ?', state).reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
 					else
-						if session[:prev] == nil
-							@articles = @articles
-							@arr = [@active_user, @articles, nil]
-					#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
+						@articles = @articles.reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
 					end
 				end
-				if filter == "salary"
-					#low and high
-					low = params["low_salary"]
-					high = params["high_salary"]
-					if (low != "undefined") && (high != "undefined")
-						if session[:prev] == nil
-							@articles = @articles.where('compensation >= ?', low).where('compensation <= ?', high)
-							@arr = [@active_user, @articles, nil]
-						#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.where('compensation >= ?', low).where('compensation <= ?', high).reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
-					elsif low != "undefined"
-						if session[:prev] == nil
-							@articles = @articles.where('compensation >= ?', low)
-							@arr = [@active_user, @articles, nil]
-						#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.where('compensation >= ?', low).reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
-					elsif high != "undefined"
-						if session[:prev] == nil
-							@articles = @articles.where('compensation >= ?', 0).where('compensation <= ?', high)
-							@arr = [@active_user, @articles, nil]
-						#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.where('compensation >= ?', 0).where('compensation <= ?', high).reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
+			end
+			if filter == "company"
+				company = params["company"]
+				if company != ""
+					session[:filter_value] = company
+					if session[:prev] == nil
+						@articles = @articles.where('company = ?', company).paginate(:page => params[:page], :per_page => 4)
+						@arr = [@active_user, @articles]
+				#if a param was previous selcted, we should kepp follwing it when going to a different page
 					else
-						if session[:prev] == nil
-							@articles = @articles
-							@arr = [@active_user, @articles, nil]
-						#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
+						@articles = @articles.where('company = ?', company).paginate(:page => params[:page], :per_page => 4).reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
+					end
+				else
+					session[:filter_by] = nil
+					if session[:prev] == nil
+						@articles = @articles.paginate(:page => params[:page], :per_page => 4)
+						@arr = [@active_user, @articles]
+				#if a param was previous selcted, we should kepp follwing it when going to a different page
+					else
+						@articles = @articles.paginate(:page => params[:page], :per_page => 4).reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
 					end
 				end
-				if filter == "company"
-					company = params["company"]
-					if company != "undefined"
-						if session[:prev] == nil
-							@articles = @articles.where('company = ?', company)
-							@arr = [@active_user, @articles, nil]
+			end
+			if filter == "industry_type"
+				industry = params["industry"]
+				if industry != ""
+					session[:filter_value] = industry
+					if session[:prev] == nil
+						@articles = @articles.where('industry_type = ?', industry)
+						@arr = [@active_user, @articles]
 					#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.where('company = ?', company).reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
 					else
-						if session[:prev] == nil
-							@articles = @articles
-							@arr = [@active_user, @articles, nil]
+						@articles = @articles.where('industry_type = ?', industry).reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
+					end
+				else
+					session[:filter_by] = nil
+					if session[:prev] == nil
+						@articles = @articles
+						@arr = [@active_user, @articles]
 					#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
-					end
-				end
-				if filter == "industry_type"
-					industry = params["industry"]
-					if industry != "undefined"
-						if session[:prev] == nil
-							@articles = @articles.where('industry_type = ?', industry)
-							@arr = [@active_user, @articles, nil]
-						#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.where('industry_type = ?', industry).reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
 					else
-						if session[:prev] == nil
-							@articles = @articles
-							@arr = [@active_user, @articles, nil]
-						#if a param was previous selcted, we should kepp follwing it when going to a different page
-						else
-							@articles = @articles.reorder(queries[session[:prev]].to_s + session[session[:prev]])
-							@arr = [@active_user, @articles, nil]
-						end
+						@articles = @articles.reorder(queries[session[:prev]].to_s + session[session[:prev]])
+						@arr = [@active_user, @articles]
 					end
 				end
-				respond_to do |format|
-    				format.html
-    				format.json if request.xhr?
-    			end
-    		end
+			end
+			if request.xhr?
+				render(partial: 'articles', locals:{articles: @arr[1]}) and return
+			end
 		else
 			#if no sorting nor filtering param is selected (in the case of going to next or prev page)
 			
 			#if no param has ever been slected (the page was loaded for the first time)
 			if session[:prev] == nil
 				@articles = Article.getApprovedArticles.paginate(:page => params[:page], :per_page => 4)
-				@arr = [@active_user, @articles, nil]	
+				@arr = [@active_user, @articles]	
 			#if a param was previous selcted, we should kepp follwing it when going to a different page
 			else
 				@articles = Article.getApprovedArticles.paginate(:page => params[:page], :per_page => 4).reorder(queries[session[:prev]].to_s + session[session[:prev]])
-				@arr = [@active_user, @articles, nil]
+				@arr = [@active_user, @articles]
 			end
 		end
 	end
@@ -277,6 +260,4 @@ class ArticlesController < ApplicationController
 			flash[:alert] = "Failed to upvote"	
 		end
 	end
-	
-	
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#User.delete_all
-#Article.delete_all
-#Comment.delete_all
-#
-#ulist = []
-#alist = []
-#clist = []
-#
-##seed user
-#ulist << User.create!(name: "Linh Tran", email: "ltran@colgate.edu", provider: "google_oauth2", uid: "100000000000000000000", displayname: "ltran", description: "Colgate senior. Into research", show_profile: true)
-#ulist << User.create!(name: "Jeehuyn Park", email: "jpark@colgate.edu", provider: "google", uid: "100000000000000000001", displayname: "jpark", description: "Colgate sophomore", show_profile: true)
-#
-##seed article 
-#alist << Article.create!(company: "Amazon", industry_type: "Tech", state: "WA", city: "Seattle", compensation: 100000, interview_exp: "Pretty simple interview", work_exp: "Great team. Challenging work", upvotes: 0, admin_approved: true,  approved: DateTime.new(2020, 11, 04, 03, 00, 00), user_id: 1)
-#
-#alist << Article.create!(company: "Uber", industry_type: "Tech", state: "CA", city: "San Jose", compensation: 90000, interview_exp: "Hard interview. System design questions", work_exp: "get work in different teams, pretty cool perks", upvotes: 0, admin_approved: true, approved: DateTime.new(2020, 11, 04, 04, 00, 00), user_id: 1)
-#
-#alist << Article.create!(company: "Pzifer", industry_type: "Medical", state: "NJ", city: "Newark", compensation: 70000, interview_exp: "Really hard interview", work_exp: "Hands on work. Learned a great deal", upvotes: 0,admin_approved: true,  approved: DateTime.new(2020, 11, 04, 05, 00, 00), user_id: 1)
-#
-#alist << Article.create!(company: "Pepsico", industry_type: "Food", state: "TX", city: "Houston", compensation: 60000, interview_exp: "Lots of behavioral questions", work_exp: "First time working in the food industry, team was very welcoming", upvotes: 0, admin_approved: true, approved: DateTime.new(2020, 11, 04, 06, 00, 00), user_id: 1)
-#
-#alist << Article.create!(company: "Goldman Sachs", industry_type: "Finance", state: "NY", city: "New York", compensation: 70000, interview_exp: "Modelling questions. Behavioral questions", work_exp: "Not so good experience, was overworked", upvotes: 0,admin_approved: true,  approved: DateTime.new(2020, 11, 04, 07, 00, 00), user_id: 2)
-#
-#alist << Article.create!(company: "Business Ink", industry_type: "Business", state: "NY", city: "New York", compensation: 40000, interview_exp: "Logic type of questions", work_exp: "Not really vibing with my team", upvotes: 0,admin_approved: true, approved: DateTime.new(2020, 11, 04, 10,  00, 00), user_id: 2)
-#
-##seed comment 
-#clist << Comment.create!(content: "Horrible, inaccurate post", upvotes: 1, downvotes: 7, user_id: 1, article_id: 1)
-#clist << Comment.create!(content: "I really think this is wrong", upvotes: 1, downvotes: 7, user_id: 1, article_id: 2)
-#clist << Comment.create!(content: "I had a good interview tho", upvotes: 1, downvotes: 7, user_id: 2, article_id: 3)
-#clist << Comment.create!(content: "Nice!! So excited to apply!!", upvotes: 1, downvotes: 7, user_id: 1, article_id: 4)
-#clist << Comment.create!(content: "I don't think that salary is accurate for NY", upvotes: 1, downvotes: 7, user_id: 2, article_id: 5)
-#clist << Comment.create!(content: "Please give me a referral...", upvotes: 1, downvotes: 7, user_id: 1, article_id: 6)
+User.delete_all
+Article.delete_all
+Comment.delete_all
+
+ulist = []
+alist = []
+clist = []
+
+#seed user
+ulist << User.create!(name: "Linh Tran", email: "ltran@colgate.edu", provider: "google_oauth2", uid: "100000000000000000000", displayname: "ltran", description: "Colgate senior. Into research", show_profile: true)
+ulist << User.create!(name: "Jeehuyn Park", email: "jpark@colgate.edu", provider: "google", uid: "100000000000000000001", displayname: "jpark", description: "Colgate sophomore", show_profile: true)
+
+#seed article 
+alist << Article.create!(company: "Amazon", industry_type: "Tech", state: "WA", city: "Seattle", compensation: 100000, interview_exp: "Pretty simple interview", work_exp: "Great team. Challenging work", upvotes: 0, admin_approved: true,  approved: DateTime.new(2020, 11, 04, 03, 00, 00), user_id: 1)
+
+alist << Article.create!(company: "Uber", industry_type: "Tech", state: "CA", city: "San Jose", compensation: 90000, interview_exp: "Hard interview. System design questions", work_exp: "get work in different teams, pretty cool perks", upvotes: 0, admin_approved: true, approved: DateTime.new(2020, 11, 04, 04, 00, 00), user_id: 1)
+
+alist << Article.create!(company: "Pzifer", industry_type: "Medical", state: "NJ", city: "Newark", compensation: 70000, interview_exp: "Really hard interview", work_exp: "Hands on work. Learned a great deal", upvotes: 0,admin_approved: true,  approved: DateTime.new(2020, 11, 04, 05, 00, 00), user_id: 1)
+
+alist << Article.create!(company: "Pepsico", industry_type: "Food", state: "TX", city: "Houston", compensation: 60000, interview_exp: "Lots of behavioral questions", work_exp: "First time working in the food industry, team was very welcoming", upvotes: 0, admin_approved: true, approved: DateTime.new(2020, 11, 04, 06, 00, 00), user_id: 1)
+
+alist << Article.create!(company: "Goldman Sachs", industry_type: "Finance", state: "NY", city: "New York", compensation: 70000, interview_exp: "Modelling questions. Behavioral questions", work_exp: "Not so good experience, was overworked", upvotes: 0,admin_approved: true,  approved: DateTime.new(2020, 11, 04, 07, 00, 00), user_id: 2)
+
+alist << Article.create!(company: "Business Ink", industry_type: "Business", state: "NY", city: "New York", compensation: 40000, interview_exp: "Logic type of questions", work_exp: "Not really vibing with my team", upvotes: 0,admin_approved: true, approved: DateTime.new(2020, 11, 04, 10,  00, 00), user_id: 2)
+
+#seed comment 
+clist << Comment.create!(content: "Horrible, inaccurate post", upvotes: 1, downvotes: 7, user_id: 1, article_id: 1)
+clist << Comment.create!(content: "I really think this is wrong", upvotes: 1, downvotes: 7, user_id: 1, article_id: 2)
+clist << Comment.create!(content: "I had a good interview tho", upvotes: 1, downvotes: 7, user_id: 2, article_id: 3)
+clist << Comment.create!(content: "Nice!! So excited to apply!!", upvotes: 1, downvotes: 7, user_id: 1, article_id: 4)
+clist << Comment.create!(content: "I don't think that salary is accurate for NY", upvotes: 1, downvotes: 7, user_id: 2, article_id: 5)
+clist << Comment.create!(content: "Please give me a referral...", upvotes: 1, downvotes: 7, user_id: 1, article_id: 6)

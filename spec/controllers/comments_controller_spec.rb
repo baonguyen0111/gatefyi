0c0800931adad48ea6a3b659da89c2ef07102143
Ubuntu <ubuntu@ip-172-31-58-123.ec2.internal>
2020-12-10 02:53:56 +0000

    fix comment tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe CommentsController, type: :controller do 
-	
+
 
 	context "index" do
 		it "routes correctly" do
@@ -10,8 +10,8 @@ RSpec.describe CommentsController, type: :controller do
 			p = Article.create!(company: "Amazon", industry_type: "Tech", state: "WA", city: "Seattle", compensation: 100000, interview_exp: "Pretty simple interview", work_exp: "Great team. Challenging work", upvotes: 10, approved: DateTime.new(2020, 11, 04, 03, 00, 00), admin_approved: true, user_id: a.id)
 			c = Comment.create!(content: "cool beans", upvotes: 1, downvotes: 0, user_id: a.id, article_id: p.id)
 
-    			get :index, :params => { :article_id => p.id }
-            		expect(response.status).to eq(200)
+			get :index, :params => { :article_id => p.id }
+			expect(response.status).to eq(200)
 		end
 
 		it "renders the index template" do
@@ -19,63 +19,76 @@ RSpec.describe CommentsController, type: :controller do
 			a = User.create!(name: "Linh Tran", email: "ltran@colgate.edu", provider: "google_oauth2", uid: "100000000000000000000", displayname: "ltran", description: "Colgate senior. Into research", show_profile: true, isAdmin: false)
 			p = Article.create!(company: "Amazon", industry_type: "Tech", state: "WA", city: "Seattle", compensation: 100000, interview_exp: "Pretty simple interview", work_exp: "Great team. Challenging work", upvotes: 10, approved: DateTime.new(2020, 11, 04, 03, 00, 00), admin_approved: true, user_id: a.id)
 			c = Comment.create!(content: "cool beans", upvotes: 1, downvotes: 0, user_id: a.id, article_id: p.id)
-    			get :index, :params => { :article_id => p.id }
-    			expect(assigns(:comments)).to match_array([c])
-    			expect(response).to render_template("index")
-		end
-	
-	end
-	
-	context "show" do
-		it "routes correctly" do
-			allow(controller).to receive(:is_logged_in?).and_return(true)
-			a = User.create!(name: "Linh Tran", email: "ltran@colgate.edu", provider: "google_oauth2", uid: "100000000000000000000", displayname: "ltran", description: "Colgate senior. Into research", show_profile: true, isAdmin: false)
-			p = Article.create!(company: "Amazon", industry_type: "Tech", state: "WA", city: "Seattle", compensation: 100000, interview_exp: "Pretty simple interview", work_exp: "Great team. Challenging work", upvotes: 10, approved: DateTime.new(2020, 11, 04, 03, 00, 00), admin_approved: true, user_id: a.id)
-			c = Comment.create!(content: "cool beans", upvotes: 1, downvotes: 0, user_id: a.id, article_id: p.id)
-    		expect(Comment).to receive(:find).with(eq("1").or eq(1)) { c }
-    		get :show, :params => { :id => 1 }
-    		expect(response.status).to eq(200)
+			get :index, :params => { :article_id => p.id }
+			expect(assigns(:comments)).to match_array([c])
+			expect(response).to render_template("index")
 		end
 
-		it "renders the show template" do
-			allow(controller).to receive(:is_logged_in?).and_return(true)
-			a = User.create!(name: "Linh Tran", email: "ltran@colgate.edu", provider: "google_oauth2", uid: "100000000000000000000", displayname: "ltran", description: "Colgate senior. Into research", show_profile: true, isAdmin: false)
-			p = Article.create!(company: "Amazon", industry_type: "Tech", state: "WA", city: "Seattle", compensation: 100000, interview_exp: "Pretty simple interview", work_exp: "Great team. Challenging work", upvotes: 10, approved: DateTime.new(2020, 11, 04, 03, 00, 00), admin_approved: true, user_id: a.id)
-			c = Comment.create!(content: "cool beans", upvotes: 1, downvotes: 0, user_id: a.id, article_id: p.id)
-			expect(Comment).to receive(:find).with(eq("1").or eq(1)) { c }
-			expect(User).to receive(:find).with(eq("1").or eq(1)) {a}
-			get :show, :params => { :id => 1 }
-			expect(assigns(:comment)).to eq(c)
-			expect(response).to render_template("show")
-		end
-		
 	end
-	
+
 	context "new" do
 		it "routes correctly" do
 			allow(controller).to receive(:is_logged_in?).and_return(true)
 			get :new
-    		expect(response.status).to eq(200)
+			expect(response.status).to eq(200)
 		end
-		
+
 		it "renders the new template" do
 			allow(controller).to receive(:is_logged_in?).and_return(true)
-            get :new
-            expect(response).to have_http_status(:success)
-        	expect(response).to render_template(:new)
-        	end
+			get :new
+			expect(response).to have_http_status(:success)
+			expect(response).to render_template(:new)
+		end
 	end
-	
+
 	context "create" do
 		it "routes correctly" do
 			allow(controller).to receive(:is_logged_in?).and_return(true)
 			a = User.create!(name: "Linh Tran", email: "ltran@colgate.edu", provider: "google_oauth2", uid: "100000000000000000000", displayname: "ltran", description: "Colgate senior. Into research", show_profile: true, isAdmin: false)
 			p = Article.create!(company: "Amazon", industry_type: "Tech", state: "WA", city: "Seattle", compensation: 100000, interview_exp: "Pretty simple interview", work_exp: "Great team. Challenging work", upvotes: 10, approved: DateTime.new(2020, 11, 04, 03, 00, 00), admin_approved: true, user_id: a.id)
 			allow(controller).to receive(:current_user).and_return(a)
-			get :create, :params => {:comment => {content: "cool beans", upvotes: 1, downvotes: 0, user_id: a.id, article_id: p.id}}
-    		expect(response).to have_http_status(:redirect)
+			get :create, :params => {:comment => {content: "cool beans"}, article_id: p.id}
+			expect(response).to have_http_status(:redirect)
 		end
 	end
-	
-	
+
+	context "update" do 
+		it "works for vote change" do 
+			allow(controller).to receive(:is_logged_in?).and_return(true)
+			a = User.create!(name: "Linh Tran", email: "ltran@colgate.edu", provider: "google_oauth2", uid: "100000000000000000000", displayname: "ltran", description: "Colgate senior. Into research", show_profile: true, isAdmin: false)
+			p = Article.create!(company: "Amazon", industry_type: "Tech", state: "WA", city: "Seattle", compensation: 100000, interview_exp: "Pretty simple interview", work_exp: "Great team. Challenging work", upvotes: 10, approved: DateTime.new(2020, 11, 04, 03, 00, 00), admin_approved: true, user_id: a.id)
+			c = Comment.create!(content: "cool beans", upvotes: 1, downvotes: 0, user_id: a.id, article_id: p.id)
+			allow(controller).to receive(:current_user).and_return(a)
+			get :update, :params => {article_id: p.id, id: c.id, :vote_change => 1}
+			expect(response).to have_http_status(:redirect)
+		end
+
+		it "works for comment change" do
+			allow(controller).to receive(:is_logged_in?).and_return(true)
+			a = User.create!(name: "Linh Tran", email: "ltran@colgate.edu", provider: "google_oauth2", uid: "100000000000000000000", displayname: "ltran", description: "Colgate senior. Into research", show_profile: true, isAdmin: false)
+			p = Article.create!(company: "Amazon", industry_type: "Tech", state: "WA", city: "Seattle", compensation: 100000, interview_exp: "Pretty simple interview", work_exp: "Great team. Challenging work", upvotes: 10, approved: DateTime.new(2020, 11, 04, 03, 00, 00), admin_approved: true, user_id: a.id)
+			c = Comment.create!(content: "cool beans", upvotes: 1, downvotes: 0, user_id: a.id, article_id: p.id)
+			allow(controller).to receive(:current_user).and_return(a)
+			get :update, :params => {article_id: p.id, id: c.id, :comment=> {:content => "new comment"}}
+			expect(response).to have_http_status(:redirect)
+
+		end
+
+	end
+
+	context "#edit" do
+		it "renders edit template" do
+			allow(controller).to receive(:is_logged_in?).and_return(true)
+			a = User.create!(name: "Linh Tran", email: "ltran@colgate.edu", provider: "google_oauth2", uid: "100000000000000000000", displayname: "ltran", description: "Colgate senior. Into research", show_profile: true, isAdmin: false)
+			p = Article.create!(company: "Amazon", industry_type: "Tech", state: "WA", city: "Seattle", compensation: 100000, interview_exp: "Pretty simple interview", work_exp: "Great team. Challenging work", upvotes: 10, approved: DateTime.new(2020, 11, 04, 03, 00, 00), admin_approved: true, user_id: a.id)
+			c = Comment.create!(content: "cool beans", upvotes: 1, downvotes: 0, user_id: a.id, article_id: p.id)
+			allow(controller).to receive(:current_user).and_return(a)
+			get :edit, :params => {article_id: p.id, id: c.id}
+			expect(assigns(:comment)).to eq(c)
+			expect(response.status).to eq(200)
+			expect(response).to render_template("edit")
+		end
+	end
+
+
 end
